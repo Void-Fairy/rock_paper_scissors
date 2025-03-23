760ead7aeefe2a5a0bf0f79339798d0c59858a89
Void Fairy <anwar.private@qubaja.com>
2025-03-23 20:49:39 +0100

    correct logic error in pseudocode computerChoice
    I want to compare humanChoice and computerChoice on the bases of
    numbers, not words, for simplicity sake. For that I changed the
    pseudcode of computerChoice from just generating a random number between
    0 and exclusive 10, to generating a number out of 1, 2 and 3.
    
    The code has not yet been implemented.

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-/*
- * GENERATE random whole number between 1 and 9
- */
-let computerChoice
 //variable for following functins
+let computerChoice
 
+/*
+ * GENERATE random whole number n [0;1)
+ * IF n < 0,33 RETURN 1
+ * ELSE IF n < 0,66 RETURN 2
+ * ELSE IF n < 1 RETURN 3
+ */
 function getComputerChoice() {
 return computerChoice = Math.random() * 10;
 }
 
+/*
+ * DISPLAY input field with string containing instructions
+ * GET human choice as variable
+ * IF case1 RETURN 1
+ * ELSE IF case2 RETURN 2
+ * ELSE IF case3 RETURN 3
+ */
