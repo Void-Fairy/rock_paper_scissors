ac002539718930f0ee1b401a42062a2d527183ca
Void Fairy <anwar.private@qubaja.com>
2025-03-25 00:02:02 +0100

    write function playGame
    I wrote the function that lets the game play five times and gives
    feedback for how often player has won or lost.
    
    Furthermore I changed the console.log entries of playRound to window.confirm.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
  * IF n < 0,33 RETURN 1
  * ELSE IF n < 0,66 RETURN 2
  * ELSE IF n < 1 RETURN 3
- */
+*/
 function getComputerChoice() {
   let randomValue = Math.random();
   if (randomValue < 0.333) {
@@ -21,7 +21,7 @@ function getComputerChoice() {
  * IF case1 RETURN 1
  * ELSE IF case2 RETURN 2
  * ELSE IF case3 RETURN 3
- */
+*/
 function getHumanChoice() {
   let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
   if (humanChoice == "rock") {
@@ -36,6 +36,7 @@ function getHumanChoice() {
 //variables for future use
 let computerScore = 0;
 let humanScore = 0;
+let tieScore = 0;
 
 /*
  * 1x = computerx
@@ -50,34 +51,43 @@ let humanScore = 0;
  * CASE 11 & 23 DISPLAY computer wins & INCRIMENT computerScore by 1
  * CASE 12 & 21 DISPLAY computer wins & INCRIMENT computerScore by 1
  * CASE 13 & 22 DISPLAY computer wins & INCRIMENT computerScore by 1
- */ 
+*/ 
 
 function playRound(computerChoice = getComputerChoice(), humanChoice = getHumanChoice()) {
   switch(true) {
     case computerChoice == humanChoice:
-      console.log("It\'s a tie!");
+      window.confirm("It\'s a tie!");
+      tieScore++;
     break;
     case computerChoice == 1 && humanChoice == 2:
     case computerChoice == 2 && humanChoice == 3:
-    case computerChoice == 3 && humanChoice == 4:
-      console.log("You win!");
+    case computerChoice == 3 && humanChoice == 1:
+      window.confirm("You win!");
       humanScore++;
       break;
     case computerChoice == 1 && humanChoice == 3:
     case computerChoice == 2 && humanChoice == 1:
     case computerChoice == 3 && humanChoice == 2:
-      console.log("You lose!");
+      window.confirm("You lose!");
       computerScore++;
       break;
   }
 }
 /*
+ *PLAYROUND 5 times,
+ IF a > b ALERT a wins
+ ELSE IF a < b ALERT a loses 
+ ELSE AlERT tied
+*/
 function playGame() {
   playRound();
   playRound();
   playRound();
   playRound();
   playRound();
-
+  pointDeclaration = `The computer has won ${computerScore} rounds, while you have ${humanScore} and you have tied ${tieScore} tiems:`;
+  winnerDeclaration = computerScore > humanScore ? "You have lost!" : computerScore < humanScore ? "You have won!" : "It\'s a tie!";
+  window.confirm(pointDeclaration + " " + winnerDeclaration);
 }
-*/
+
+playGame();
